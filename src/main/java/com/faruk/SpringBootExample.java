e1662aaf6291cbbf5c73c7a6f78400b36d6fe7aa
Mr. Developer <41997267+denemehesabim@users.noreply.github.com>
2020-10-18 00:54:02 +0300

    Update SpringBootExample.java

--- src/main/java/com/faruk/SpringBootExample.java
+++ src/main/java/com/faruk/SpringBootExample.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootExample {
+	
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringBootExample.class, args);
